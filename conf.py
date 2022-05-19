75bd9b13c7827d5dce3729ec71bd4c012b4fc1d3
diago8888 <102332780+diago8888@users.noreply.github.com>
2022-05-19 16:46:20 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,4 +2,3 @@ api_hash = "ebb09446c3def9fc6c69f1a10e1252a0"
 api_id = 12729863
 bot_token = "5385330314:AAFfA2gLPs9KhprRyyFKgQbTZ33w0E1FPFI"
 bot_admin_group = -1001524438621
-bot_savedata_group = -1001624299524
