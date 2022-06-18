dc74362cfab15f632c4a871bab49a2532ecc383d
zeta30 <102247888+zeta30@users.noreply.github.com>
2022-06-18 09:38:30 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "ebb09446c3def9fc6c69f1a10e1252a0"
-api_id = 12729863
-bot_token = "5528432307:AAHeQ8BbOoTsw2O31r4uNbH4JS0W4Rc0-B0"
-bot_admin_group = -1001723447167
+api_hash = "e7681747482f5867af9de8171136a89b"
+api_id = 17897679
+bot_token = "5430591151:AAHlhj_srlVDLcwYoIolOnc5FtnPgKIoWjw"
+bot_admin_group = -747624969
