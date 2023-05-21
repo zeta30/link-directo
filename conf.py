4e56be456e013d2b4a83ef7a602102ddc622728d
zeta30 <102247888+zeta30@users.noreply.github.com>
2023-05-21 06:41:40 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "e7681747482f5867af9de8171136a89b"
 api_id = 17897679
-bot_token = "5430591151:AAHlhj_srlVDLcwYoIolOnc5FtnPgKIoWjw"
+bot_token  =  "6174925017:AAEFbQNERzezwwuISYblhrIvtnO-4CThLhM"
 bot_admin_group = -747624969
